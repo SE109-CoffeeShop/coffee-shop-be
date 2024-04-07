26e87abe938592234fe10a6051049442488902e6
nb150301 <anhnguyen150301@gmail.com>
2024-04-07 13:56:35 +0700

    product controller CRUD

---
+++ app/Http/Controllers/Worker/ProductController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \App\Http\Requests\StoreUserRequest $request
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \App\Http\Requests\UpdateUserRequest $request
+     * @param \App\Models\User                     $user
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+}
