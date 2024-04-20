2200fe7fc889c597c2b557a69dced023eeef17c2
nb150301 <anhnguyen150301@gmail.com>
2024-04-20 20:03:53 +0700

    sender in wrap up

--- app/ConfigurationsApp/WrapUp.php
+++ app/ConfigurationsApp/WrapUp.php
@@ -3,6 +3,8 @@
 namespace App\ConfigurationsApp;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WrapUp
 {
@@ -17,5 +19,27 @@ class WrapUp
         $user->delete();
 
         return response("", 204);
+    }
+
+    public function getForms(Request $request) {
+        $request->validate([
+            'id' => 'required',
+        ]);
+        $employeeId = $request->input('id');
+
+        $forms = DB::table('request_form')->where('sender_id', $employeeId)->get()->toArray();
+
+        $data = [];
+        foreach ($forms as $form) {
+            $data[] = [
+                'id' => $form->id,
+                'reason' => $form->reason,
+                'start_date' => date('d/m/Y', strtotime($form->start_date)),
+                'end_date' => date('d/m/Y', strtotime($form->end_date)),
+                'status' => $form->status,
+            ];
+        }
+
+        return response(json_encode($data));
     }
 }
